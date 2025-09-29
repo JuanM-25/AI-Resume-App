7d2472222f144ac2205cd6a604b56cad55861054
Juan Diego Marin Herrera <94065036+JuanM-25@users.noreply.github.com>
2025-09-29 17:04:04 -0600

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -62,6 +62,7 @@ const Upload = () => {
       await kv.set(`resume-${uuid}`, JSON.stringify(data));
       setStatusText('Analysis complete! Redirecting...');
       console.log(data);
+      navigate(`/resume/${uuid}`);
 
     }
 
